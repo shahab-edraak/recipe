5b1ccac5fc5c64e3a96ef151cba4e6a7c12fefcc
shahab-edraak <shahab@edraaksystems.com>
2023-09-08 18:42:34 +0500

    scss added for app and media queries

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { ImSpoonKnife } from 'react-icons/im';
 
 const Header = () => {
     return (
-        <header className='header' style={{ backgroundImage: `https://images.unsplash.com/photo-1543339308-43e59d6b73a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80` }}>
+        <header className='header' style={{ backgroundImage: `url(https://images.unsplash.com/photo-1543339308-43e59d6b73a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)` }}>
             <div className='layer'>
                 <div className="container">
                     <nav className='logo-wrapper'>
